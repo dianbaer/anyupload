f715d7e8f82e7abb5205355fbfcc782b96af2e65
dianbaer <232365732@qq.com>
2018-03-09 21:14:06 +0800

    11

--- dist/AnyUploadClient/js/anyupload/src/FileSystemStatus.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-    if (!window.anyupload) window.anyupload = {};
-    var FileSystemStatus = function () {
-        this.nowFoldId = "rootId";
-        this.nowFoldName = "root";
-    };
-    window.anyupload.fileSystemStatus = new FileSystemStatus();
-})(window);
